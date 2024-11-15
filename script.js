3ac598c0f20ce749f56fff61bdc90d590c27bf07
Cam <112639963+CameronMeile@users.noreply.github.com>
2024-11-15 22:53:08 +0100

    🚧 chore: Tiny Steps for AddTask();

---
+++ script.js
@@ -0,0 +1,7 @@
+let AddTaskNameValue = document.getElementById('taskName').value;
+let AddTaskDescValue = document.getElementById('taskDesc').value;
+let AddTaskIsDoneValue = document.getElementById('isDone').value;
+
+function AddTask() {
+    console.log(AddTaskNameValue, AddTaskDescValue, AddTaskIsDoneValue);
+}
