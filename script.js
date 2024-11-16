bf655e84773c923c896e84e8030be58122ac56f2
Cam <chemibrothers@bluewin.ch>
2024-11-16 23:00:45 +0100

    🚧 chore: Added AddTask();

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-let AddTaskNameValue = document.getElementById('taskName').value;
-let AddTaskDescValue = document.getElementById('taskDesc').value;
-let AddTaskIsDoneValue = document.getElementById('isDone').value;
+let CurrentTaskId = 1;
+let TaskList = document.querySelector("#List table tbody");
 
 function AddTask() {
-    console.log(AddTaskNameValue, AddTaskDescValue, AddTaskIsDoneValue);
+  let ErrorMessage = "";
+  let IsError = false;
+
+  let AddTaskNameValue = document.getElementById("taskName").value;
+  let AddTaskDescValue = document.getElementById("taskDesc").value;
+  let AddTaskIsDoneValue = document.getElementById("isDone");
+
+  if (AddTaskNameValue == "") {
+    ErrorMessage += "- Task Name wurde nicht eingetragen! \n";
+    IsError = true;
+  }
+
+  if (AddTaskDescValue == "") {
+    ErrorMessage += "- Task Desc wurde nicht eingetragen! \n";
+    IsError = true;
+  }
+
+  if (!IsError) {
+    let newTask = document.createElement("tr");
+    let taskID = document.createElement("td");
+    let taskName = document.createElement("td");
+    let taskDesc = document.createElement("td");
+    let taskIsDone = document.createElement("td");
+
+    taskID.innerText = CurrentTaskId;
+    taskName.innerText = AddTaskNameValue;
+    taskDesc.innerText = AddTaskDescValue;
+
+    if (AddTaskIsDoneValue.checked) {
+      taskIsDone.innerHTML = `<input type="checkbox" checked>`;
+    } else {
+      taskIsDone.innerHTML = `<input type="checkbox">`;
+    }
+
+    newTask.appendChild(taskID);
+    newTask.appendChild(taskName);
+    newTask.appendChild(taskDesc);
+    newTask.appendChild(taskIsDone);
+    TaskList.appendChild(newTask);
+
+    CurrentTaskId++;
+  } else {
+    alert(ErrorMessage);
+  }
 }
